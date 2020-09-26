bcddf0cf1cfa2124de3a41fb1800ec4e2e05a63b
aashiqui <40266375+aashiqui@users.noreply.github.com>
2020-09-26 12:03:30 +0530

    Create proxy.sh

---
+++ proxy.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+clear
+
+rm -rf ubunut-proxy.conf
+wget https://raw.githubusercontent.com/aashiqui/content/master/ubunut-proxy.conf
+echo "Please provide your domain name without the www. (e.g. mydomain.com)"
+read -p "Type your domain name, then press [ENTER] : " MY_DOMAIN
+echo "Please provide a Proxy IP "
+read -p "Type your IP, then press [ENTER] : " IP
+
+read -t 30 -p "Thank you. Please press [ENTER] continue or [Control]+[C] to cancel"
+echo "Provided Domain : $MY_DOMAIN and IP : $IP"
+
+
+if [ -f /etc/redhat-release ]; then
+  echo "Detected System is Centos"
+  touch /etc/nginx/conf.d/${MY_DOMAIN}.conf
+  sed "s/ip_name/$IP/g;s/host_name/$MY_DOMAIN/g;" ubunut-proxy.conf > /etc/nginx/conf.d/${MY_DOMAIN}.conf
+
+  
+fi
+
+if [ -f /etc/lsb-release ]; then
+    echo "Detected System is Ubuntu"
+	 touch /etc/nginx/sites-available/${MY_DOMAIN}.conf
+  sed "s/ip_name/$IP/g;s/host_name/$MY_DOMAIN/g;" ubunut-proxy.conf > /etc/nginx/sites-available/${MY_DOMAIN}.conf
+
+fi
